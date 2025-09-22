d15ff93abe9a5a2c6c9c4dd8f3a90240bab44d03
Juan Gomes <juanhgomes@gmail.com>
2025-09-22 10:53:39 -0300

    Migração de Maven para Gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "agimob-v1"
---
+++ src/main/java/com/example/agimob_v1/AgimobV1Application.java
@@ -0,0 +1,13 @@
+package com.example.agimob_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgimobV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgimobV1Application.class, args);
+	}
+
+}
