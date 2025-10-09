1db07c980fc867851f058b332055782600d73650
Ana Carol <carolana.c40@gmail.com>
2025-10-09 08:57:11 -0300

    Configuracao de variaveis de ambiente

--- src/main/java/com/example/agimob_v1/model/Usuario.java
+++ src/main/java/com/example/agimob_v1/model/Usuario.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Usuario {
+public class rv dUsuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
