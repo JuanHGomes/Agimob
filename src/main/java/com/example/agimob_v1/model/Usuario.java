203bc9c9741e3976f18ba5766d7d6caa73a800d3
Juan Gomes <150093486+JuanHGomes@users.noreply.github.com>
2025-10-10 08:38:12 -0300

    Correção entidade Usuario
    Correção de bug na entidade usuário.

--- src/main/java/com/example/agimob_v1/model/Usuario.java
+++ src/main/java/com/example/agimob_v1/model/Usuario.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class rv dUsuario {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
